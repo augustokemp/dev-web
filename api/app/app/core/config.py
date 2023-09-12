f157904788a533aebe22d004743febd99999b2fc
Kemp <augusto.kemp@gmail.com>
2023-09-12 19:21:13 -0300

    ajustes .env

--- api/app/app/core/config.py
+++ api/app/app/core/config.py
@@ -14,8 +14,8 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
-    PROJECT_NAME = "dev-web"
-    TAG = "DEV"
+    PROJECT_NAME: str
+    TAG: str = "DEV"
 
     class Config:
         case_sensitive = True
